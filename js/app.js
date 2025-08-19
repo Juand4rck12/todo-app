e8712d29987f5c14b8dfe48fd0e610bd107a7c2b
Juan_Diego <juan3312orrego@gmail.com>
2025-08-19 10:58:49 -0500

    Todo App - v1
    Primera versión funcional y organizada, con almacenamiento en localStorage

--- js/app.js
+++ js/app.js
@@ -14,7 +14,11 @@ function addTodo() {
     const todoText = todoInput.value.trim();
 
     if (todoText.length > 0) {
-        allTodos.push(todoText);
+        const todoObject = {
+            text: todoText,
+            completed: false
+        }
+        allTodos.push(todoObject);
         updateTodoList();
         saveTodos();
         todoInput.value = "";
@@ -32,6 +36,7 @@ function updateTodoList() {
 function createTodoItem(todo, todoIndex) {
     const todoId = "todo-" + todoIndex;
     const todoLI = document.createElement("li");
+    const todoText = todo.text;
     todoLI.className = "todo";
     todoLI.innerHTML = `
         <input type="checkbox" id="${todoId}">
@@ -41,7 +46,7 @@ function createTodoItem(todo, todoIndex) {
                     </svg>
                 </label>
                 <label for="${todoId}" class="todo-text">
-                    ${todo}
+                    ${todoText}
                 </label>
                 <button class="delete-button">
                     <svg fill="var(--secondary-color)" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"
@@ -55,6 +60,13 @@ function createTodoItem(todo, todoIndex) {
     deleteButton.addEventListener("click", () => {
         deleteTodoItem(todoIndex);
     })
+    const checkbox = todoLI.querySelector("input");
+    checkbox.addEventListener("change", () => {
+        allTodos[todoIndex].completed = checkbox.checked;
+        saveTodos();
+    })
+    checkbox.checked = todo.completed;
+
     return todoLI;
 }
 
