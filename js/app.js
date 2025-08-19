8dd53d25e7fdd957158993cc9827fbcea12d4e92
Juan_Diego <juan3312orrego@gmail.com>
2025-08-19 13:17:33 -0500

    Optimización de svg -> iconos

--- js/app.js
+++ js/app.js
@@ -41,19 +41,13 @@ function createTodoItem(todo, todoIndex) {
     todoLI.innerHTML = `
         <input type="checkbox" id="${todoId}">
                 <label class="custom-checkbox" for="${todoId}">
-                    <svg fill="transparent" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"
-                        fill="#000000"> <path d="M0 0h24v24H0V0z" fill="none" /> <path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z" />
-                    </svg>
+                    <i class="fas fa-check"></i>
                 </label>
                 <label for="${todoId}" class="todo-text">
                     ${todoText}
                 </label>
                 <button class="delete-button">
-                    <svg fill="var(--secondary-color)" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"
-                        fill="#000000"> <path d="M0 0h24v24H0V0z" fill="none" />
-                        <path
-                            d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z" />
-                    </svg>
+                    <i class="fas fa-trash"></i>
                 </button>
     `
     const deleteButton = todoLI.querySelector(".delete-button");
